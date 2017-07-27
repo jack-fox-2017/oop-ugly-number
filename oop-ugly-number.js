a8eb6133525c1a7f8432f8c875dd4e4005e44d84
dimasgardenia <dimas.gardenia@gmail.com>
2017-07-27 16:21:37 +0700

    oop-ugly-number

--- oop-ugly-number.js
+++ oop-ugly-number.js
@@ -31,12 +31,50 @@ class UglyNumber {
     this.arrUgly = [];
   }
 
+  maxDivides(number, divisible){
+    while(number % divisible == 0) {
+      number = number / divisible;
+    }
+    return number;
+  }
+
+  isUgly(numberUgly){
+    numberUgly = this.maxDivides(numberUgly, 2);
+    numberUgly = this.maxDivides(numberUgly, 3);
+    numberUgly = this.maxDivides(numberUgly, 5);
+      return (numberUgly == 1) ? true : false;
+  }
+
+  getUglyNo(input){
+    while(input>this.nowNumber){
+    this.deret++;
+      if (this.isUgly(this.deret) == 1){
+        this.nowNumber++;
+      }
+    }
+    return this.deret;
+  }
+
+  getUglyArray(input){
+    while(input > this.nowNumber){
+      this.deret++
+        if (this.isUgly(this.deret) == 1){
+            this.nowNumber++;
+            this.arrUgly.push(this.deret);
+        }
+
+
+  }
+  return this.arrUgly;
+
+  }
 }
+
 
 var ugly = new UglyNumber();
 var uglyarray = new UglyNumber();
 
-console.log(ugly.getUglyNo(25));
+console.log(ugly.getUglyNo(7));
 
 console.log(ugly.isUgly(14));
 console.log(ugly.isUgly(45));

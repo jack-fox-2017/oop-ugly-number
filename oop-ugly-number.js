e9dbdf1d4f65e4ab9dd353e8bd32b08fadabad02
aridwia <aridwiananto@gmail.com>
2017-07-27 20:46:17 +0700

    oop done

--- oop-ugly-number.js
+++ oop-ugly-number.js
@@ -31,16 +31,72 @@ class UglyNumber {
     this.arrUgly = [];
   }
 
-}
+  maxDivides(number, divisible) {
+      while(number % divisible == 0) {
+        number = number / divisible;
+      }
+      return number;
+  }
+
+  isUgly(numberUgly) {
+    numberUgly = this.maxDivides(numberUgly, 2);
+    numberUgly = this.maxDivides(numberUgly, 3);
+    numberUgly = this.maxDivides(numberUgly, 5);
+
+    return (numberUgly == 1) ? true : false;
+  }
+
+  getUglyNo(input) {
+    // let this.deret = 1;
+    // let this.nowNumber = 1; // ugly number count
+
+    while(input > this.nowNumber) {
+      this.deret++;
+      //console.log( this.arrUgly );
+      if(this.isUgly(this.deret) == 1) {
+        this.nowNumber++;
+      }
+    }
+    return this.deret;
+  }
+
+  getUglyArray(masukan){
+
+        while(masukan >= this.nowNumber) {
+
+
+//          console.log( this.arrUgly );
+          if(this.isUgly(this.deret) == 1) {
+            this.nowNumber++;
+              this.arrUgly.push(this.deret)
+          }
+          this.deret++;
+        }
+        return this.arrUgly;
+      }
+
+  }
+
+
 
 var ugly = new UglyNumber();
 var uglyarray = new UglyNumber();
+var nama = new UglyNumber();
 
-console.log(ugly.getUglyNo(25));
+
+console.log(ugly.getUglyNo(150));
+console.log(nama.getUglyNo(7));
+console.log(nama.getUglyNo(10));
+console.log(nama.getUglyNo(15));
+
+
+//console.log(ugly.getUglyNo());
+
 
 console.log(ugly.isUgly(14));
 console.log(ugly.isUgly(45));
 console.log(ugly.isUgly(57));
 
-console.log(uglyarray.getUglyArray(12));
+
+console.log(uglyarray.getUglyArray(7));
 //driver code
